2737838b849604cee72c8116c5e9af948edaca47
EduardoSpode <Eduardo.Spode01@edu.pucrs.b>
2021-04-18 18:38:37 -0300

    Atuali

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,31 @@
 
+public class Main
+{
+    public static void main(String args[])
+    {
+        Heap heap = new Heap();
+        Leitura leitura = new Leitura();
+        heap.start();
+
+        heap.insert("X0");
+        heap.insert("X1");
+        heap.insert("X2");
+        heap.insert("X3");
+        heap.insert("X4");
+        heap.insert("X5");
+        heap.insert("X6");
+        heap.insert("6");
+        heap.insert("7");
+        heap.insert("8");
+        heap.insert("8");
+        heap.insert("7");
+        heap.insert("7");
+        heap.insert("7");
+        heap.insert("8");
+        
+
+        heap.verify();
+
+        System.out.println("\nO total de nodos equilibrados foi de "+ heap.getCount());
+    }
+}
